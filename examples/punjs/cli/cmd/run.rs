b0922e369e4c4fa1706892af29904a408817e225
Gonzalo Larralde <gonzalolarralde@gmail.com>
2023-08-15 01:57:49 -0400

    Fix punjs example.

--- examples/punjs/cli/cmd/run.rs
+++ examples/punjs/cli/cmd/run.rs
@@ -7,11 +7,11 @@ use std::path::PathBuf;
 pub fn run(input: PathBuf) -> Result<()> {
     let script = fs::read_to_string(&input)
         .with_context(|| format!("Failed to load module `{}`", input.display()))?;
-    let mut context = JSContext::new();
+    let context = JSContext::new();
     setup_prelude(&context);
-    let _ = context.evaluate_script(&script, 1);
-    if let Some(ex) = context.get_exception() {
-        anyhow::bail!("Uncaught {}", ex.to_string(&context));
+    let result = context.evaluate_script(&script, 1);
+    if let Err(ex) = result {
+        anyhow::bail!("Uncaught {}", ex.to_string(&context).unwrap());
     }
     Ok(())
 }
@@ -19,19 +19,21 @@ pub fn run(input: PathBuf) -> Result<()> {
 fn setup_prelude(context: &JSContext) {
     let require_fn = JSValue::callback(&context, Some(require));
     // require()
-    let mut global = context.get_global_object();
-    global.set_property(&context, "require".to_string(), require_fn);
+    let global = context.get_global_object();
+    let _ = global.set_property(&context, "require".to_string(), require_fn);
     // foo()
     let callback = JSValue::callback(&context, Some(foo));
-    global.set_property(&context, "foo".to_string(), callback);
+    let _ = global.set_property(&context, "foo".to_string(), callback);
 }
 
 #[callback]
-fn require(_context: JSContext) {
-    println!("warning: `require` is not implemented.")
+fn require(_context: JSContext, _function: JSObject, _this: JSObject, _args: &[JSValue]) -> Result<JSValue, JSValue> {
+    println!("warning: `require` is not implemented.");
+    Ok(JSValue::undefined(&_context))
 }
 
 #[callback]
-fn foo(_context: JSContext) {
+fn foo(_context: JSContext, _function: JSObject, _this: JSObject, _args: &[JSValue]) -> Result<JSValue, JSValue> {
     println!("hello from Rust land!");
+    Ok(JSValue::undefined(&_context))
 }
